49d9e3c410a1bc9d942797552c440753dbdb04ec
Brad <117415722+bktpp@users.noreply.github.com>
2023-02-06 16:11:19 -0600

    styling changes added color picker buttons, and box shadow effects to buttons

--- main.js
+++ main.js
@@ -18,18 +18,32 @@ for (let i = 0; i < requestedDivs; i++) {
     babyDivs.style.borderRadius = '50%';
     gridContainer.insertAdjacentElement('beforeend', babyDivs);
     babyDivs.addEventListener('mouseover', function() {
-    babyDivs.style.backgroundColor = getRandomColor();}) 
-    babyDivs.addEventListener('touchstart', function() {
-    babyDivs.style.backgroundColor = getRandomColor();})    
+    babyDivs.style.backgroundColor = getRandomColor()}) 
   }
+  
 } 
 
+function colorPick(buttonClicked) {
+    let color = 'white';
+    if(buttonClicked === 'red') {
+        color = 'red';
+    } else if (buttonClicked === 'blue') {
+        color = 'blue';
+    } else if (buttonClicked === 'colorful'){
+        
+    } 
+      return color
+}
+
 function getRandomColor() {
-    let r = Math.floor(Math.random() * 256);
-    let g = Math.floor(Math.random() * 256);
-    let b = Math.floor(Math.random() * 256);
-    return "rgb(" + r + ", " + g + ", " + b + ")";
-  }
+        let r = Math.floor(Math.random() * 256);
+        let g = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256);
+        return  "rgb(" + r + ", " + g + ", " + b + ")";
+    }
+ 
+
+  
 
    function clearBoard() {
     let divs = document.querySelectorAll('.dots');
@@ -38,6 +52,32 @@ function getRandomColor() {
     })
   }
 
+  
+
+  function redColor() {
+    let red = document.querySelectorAll('.dots');
+    red.forEach(function(red2) {
+        red2.addEventListener('mouseover', function() {
+            red2.style.backgroundColor = 'red';});       
+    })
+  }
+
+  function blackColor() {
+    let black = document.querySelectorAll('.dots');
+    black.forEach(function(black2) {
+        black2.addEventListener('mouseover', function() {
+            black2.style.backgroundColor = 'black';}); 
+    })
+  } 
+
+  function colorful() {
+    let random = document.querySelectorAll('.dots');
+    random.forEach(function(random2) {
+        random2.addEventListener('mouseover', function() {
+         random2.style.backgroundColor = getRandomColor();})
+    })
+  } 
+  
 
 
 
